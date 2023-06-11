c7096ff8fade3eb867067a1416bce45104bd3899
fandi-ahmad <fandi4160@gmail.com>
2023-06-11 14:31:55 +0800

    add cors origin

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ const port = 8000
 const router = require('./routes/router')
 const { config } = require('dotenv')
 const cookieParser = require('cookie-parser')
+const cors = require('cors')
 
 config()
+app.use(cors({Credential: true, origin: process.env.USER_URL_ADDRESS}))
 app.use(cookieParser())
 app.use(express.json())
 app.use(router)
